241aa5ad5afeba585695a0a04888d0f9552c0829
RACUN13 <125272048+RACUN13@users.noreply.github.com>
2023-03-31 01:32:20 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,17 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+  int left = 0;
+  int middle;
+  int right = size - 1;
+  while (left <= right) {
+    middle = (left + right) / 2;
+    if (arr[middle] == value)
+      return 1;
+    if (arr[middle] < value)
+      left = middle + 1;
+    else
+      right = middle - 1;
+  }
+  return 0;
 }
